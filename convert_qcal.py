26dc974cf1c3d2c065a4c4dcd6daa22dbdd42303
Emil Ljungberg <ljungberg.emil@gmail.com>
2024-03-21 10:03:36 +0100

    fix infinite read

--- convert_qcal.py
+++ convert_qcal.py
@@ -24,16 +24,13 @@ def str2arr(s):
     return ss
 
 def parse_study_params(fname, protocol_number):
-    finished = False
     with open(fname, 'r') as f:
-        while not finished:
-            l = f.readline()
+        for l in f:
             s = str2arr(l)
             if s[0] == "Study Parameters":
                 params = s[1:]
                 l = f.readline()
                 values = str2arr(l)[1:]
-                finished = True
     
     D = {k:x for k,x in zip(params, values)}
     df = pd.DataFrame(D, index=[0])
@@ -41,10 +38,8 @@ def parse_study_params(fname, protocol_number):
     return df
 
 def parse_ADC_voi(fname, protocol_number):
-    finished = False
     with open(fname, 'r') as f:
-        while not finished:
-            l = f.readline()
+        for l in f:
             s = str2arr(l)
             if s[0] == "ADC VOI Statistics":
                 col_names = s[1:]
@@ -56,9 +51,7 @@ def parse_ADC_voi(fname, protocol_number):
                 VOIs = []
                 for i in range(14):
                     VOIs.append(str2arr(f.readline())[1:])
-
-                finished = True
-   
+ 
     df = pd.DataFrame(columns=col_names)
     for i in range(len(VOIs)):
         df.loc[len(df)] = [np.nan if len(x)==0 else x for x in VOIs[i]]
@@ -75,10 +68,8 @@ def parse_ADC_voi(fname, protocol_number):
     return df
 
 def parse_T2_voi(fname, protocol_number):
-    finished = False
     with open(fname, 'r') as f:
-        while not finished:
-            l = f.readline()
+        for l in f:
             s = str2arr(l)
             if s[0] == "T2 Contrast VOI Statistics":
                 col_names = s[1:]
@@ -90,8 +81,6 @@ def parse_T2_voi(fname, protocol_number):
                 VOIs = []
                 for i in range(28):
                     VOIs.append(str2arr(f.readline())[1:])
-
-                finished = True
    
     df = pd.DataFrame(columns=col_names)
     for i in range(len(VOIs)):
@@ -114,27 +103,35 @@ def parse_temp(fname, protocol_number):
             l = f.readline()
             s = str2arr(l)
             if s[0] == "Additional Calculations":
-                l2 = f.readline().split('","')[-2:]
-                temps = [float(x.strip().replace('"', '')) for x in l2]
+                l2 = f.readline().split('","')
+                temps = []
+                temp_labels = []
+                for i in range(len(s)):
+                    if 'Temperature' in s[i]:
+                        temps.append(float(l2[i]))
+                        temp_labels.append(s[i])
                 finished = True
     
-    df = pd.DataFrame(columns=['Protocol Number', s[3], s[4]])
-    df.loc[len(df)] = [int(protocol_number), temps[0], temps[1]]
+    df = pd.DataFrame(columns=['Protocol Number'] + temp_labels)
+    df.loc[len(df)] = [int(protocol_number)] + temps
     df['Protocol Number'] = df['Protocol Number'].astype(int)
     return df
 
 
 def convert_to_xls(fname, out_fname, protocol_number):
     info = parse_study_params(fname, protocol_number)
+    print(info)
     ADC = parse_ADC_voi(fname, protocol_number)
     ADC['Study Date'] = info['Study Date'][0]
+    print('Parsed ADC')
     
     temps = parse_temp(fname, protocol_number)
     temps['Study Date'] = info['Study Date'][0]
+    print('parsed temps')
 
     t2voi = parse_T2_voi(fname, protocol_number)
     t2voi['Study Date'] = info['Study Date'][0]
-    
+    print('Parsed t2vi')
     writer = pd.ExcelWriter(out_fname)
 
     ADC.to_excel(writer, sheet_name='ADC', index=False)
